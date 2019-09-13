459944b735a267fe6b3de9446bc62dfc9f2a29f7
Nathan Arthur <narthur@preemptive.com>
2019-09-13 14:02:51 -0400

    Better welcome message

--- lib/KidsBank/Controller/Example.pm
+++ lib/KidsBank/Controller/Example.pm
@@ -6,7 +6,7 @@ sub welcome {
   my $self = shift;
 
   # Render template "example/welcome.html.ep" with message
-  $self->render(msg => 'Welcome to the Mojolicious real-time web framework!');
+  $self->render(msg => 'KidsBank');
 }
 
 1;
